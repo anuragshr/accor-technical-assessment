c8093d1e2423920dfd37ef24c9bb01a0f9fba44c
anuragshr <anuragshr@yahoo.co.in>
2026-08-18 14:59:22 +0530

    Document Terraform helm state config
    Add comments explaining that the Helm module reads values from the root module's Terraform state, and note the required updates when the state file location or backend changes.

--- Code/terraform/helm/main.tf
+++ Code/terraform/helm/main.tf
@@ -1,3 +1,7 @@
+#Calls the terraform state file of a root module for values.
+#if path to other root module state file is changed then it needs to updated here as well.
+#if remote backed is used for state file, update the backend to s3 in the data block and give the path to the state file.
+
 data "terraform_remote_state" "cluster" {
   backend = "local"
   config = {
